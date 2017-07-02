8bbd5e5eb0aac686d0e57ac90f5effacf526b8e3
Kio <kio@fx-s.pro>
2017-07-03 02:08:03 +0300

    new: add full test case

--- test/ex_rsagen_test.exs
+++ test/ex_rsagen_test.exs
@@ -14,4 +14,14 @@ defmodule RsaKeysTest do
   test "test rsagen badargs" do
     assert {:error, :bad_args} == RsaKeys.rsagen(123)
   end
+
+  test "test full test case" do
+    password = "12345"
+    test_data = "test data for encrypt/decrypt!"
+
+    assert {:ok, pub_key, priv_key} = RsaKeys.generate_keys(password)
+    data = RsaKeys.encrypt_data(test_data, pub_key)
+    assert test_data == RsaKeys.decrypt_data(data, password, priv_key)
+  end
+
 end
